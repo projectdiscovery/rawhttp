3582c0c9ea3eeee4ace5ab1fbe000fcef0fafe64
Ice3man <nizamulrana@gmail.com>
2021-06-03 10:43:39 +0530

    Fixed Dor function with URL

--- client.go
+++ client.go
@@ -68,7 +68,7 @@ func (c *Client) Do(req *http.Request) (*http.Response, error) {
 func (c *Client) Dor(req *retryablehttp.Request) (*http.Response, error) {
 	method := req.Method
 	headers := req.Header
-	url := req.RequestURI
+	url := req.URL.String()
 	body := req.Body
 
 	return c.DoRaw(method, url, "", headers, body)
