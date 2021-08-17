39efb98a126eb6c558fa2bdfcd420f13302ffe62
mzack <marco.rivoli.nvh@gmail.com>
2021-08-17 19:05:53 +0200

    Adding custom tls implementation

---
+++ example/http/client.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+
+	"github.com/projectdiscovery/rawhttp/tls"
+
+	"github.com/projectdiscovery/rawhttp/http"
+)
+
+func main() {
+	client := http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+		},
+	}
+	req, _ := http.NewRequest(http.MethodGet, "http://localhost:8000", nil)
+	req.Header["zoccola puttana"] = []string{"puzzola"}
+	req.Unsafe = true
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(resp)
+}
